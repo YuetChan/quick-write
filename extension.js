365e2055f504a5d155f51ba417f4838fc16c0f26
Yuet Chan <yuetcheukchan@gmail.com>
2023-04-20 13:41:47 -0400

    Added clear cache imple

--- extension.js
+++ extension.js
@@ -1,27 +1,96 @@
 const vscode = require('vscode');
-const { v4: uuidv4 } = require('uuid');
 
-let configCache = null
+const { v4: uuidv4 } = require('uuid');
+const path = require('path');
+
+let panelId = null
 
 let globalPanel = null
 
-function init_panel(context) {
+let workspaceDir = null
+
+let dataJson = null
+
+let currentRelativePath = null
+
+
+vscode.window.onDidChangeActiveTextEditor(editor => {
+    if(editor && editor.document && globalPanel) {
+        currentRelativePath = path.relative(workspaceDir.fsPath, editor.document.fileName)
+        postCurrentFilenameAndData(`./${currentRelativePath}`, dataJson[`./${currentRelativePath}`])
+    }
+});
+
+
+async function showFileDialog() {
+    const options = vscode.OpenDialogOptions = {
+      canSelectFiles: true,
+      canSelectFolders: false,
+      canSelectMany: false,
+      
+      openLabel: 'Select',
+      filters: {
+        'Json Files': ['json']
+      },
+      title: 'Select a datasource file'
+    };
+  
+    const result = await vscode.window.showOpenDialog(options);
+  
+    if (result && result.length > 0) {
+      const fileUri = result[0];
+      vscode.window.showInformationMessage(`Selected file: ${fileUri.fsPath}`);
+
+      return fileUri.fsPath
+    } else {
+      vscode.window.showInformationMessage('No file selected.');
+      return null
+    }
+}
+
+
+function saveFile(uri, data) {
+    vscode.workspace.fs.writeFile(uri, data).then(() => {
+        console.log('Save file successfully')
+    }, (error) => {
+       console.error(error)
+    });
+}
+  
+
+function postCurrentFilenameAndData(fname, data) {
+    globalPanel.webview.postMessage({
+        command: 'activeFileChange',
+        data: {
+            'filename': fname,
+            'data': data? data : ''
+        }
+    });
+}
+
+
+function initPanel(context) {
     const panel = vscode.window.createWebviewPanel(
         panelId, 
         'Code Meta Note', 
-        vscode.ViewColumn.Two, 
+        vscode.ViewColumn.One, 
         {
             enableScripts: true,
-            retainContextWhenHidden: true
+            retainContextWhenHidden: true,
+            enableFindWidget: true
         }
     );
 
     panel.webview.html = `
     <html>
     <body>
+        <h4>File: <span id="editor-name"></span></h4>
         <textarea id="editor" style="width:600px;height:600px"></textarea>
+
         <script>
             const vscode = acquireVsCodeApi();
+
+            const name = document.getElementById('editor-name');
 
             const editor = document.getElementById('editor');
 
@@ -31,7 +100,15 @@ function init_panel(context) {
                     text: event.target.value
                 });
             });
-            
+
+            window.addEventListener('message', event => {
+                const msg = event.data;
+
+                if(msg.command == 'activeFileChange') {
+                    name.textContent = msg.data.filename
+                    editor.value = msg.data.data
+                }
+            });
         </script>
     </body>
     </html>
@@ -39,57 +116,101 @@ function init_panel(context) {
 
     globalPanel = panel
 
+    globalPanel.onDidDispose(() => {
+        globalPanel = null
+    });
+
+    const activeEditor = vscode.window.activeTextEditor
+
+    currentRelativePath = path.relative(workspaceDir.fsPath, activeEditor.document.fileName);
+    postCurrentFilenameAndData(`./${currentRelativePath}`, dataJson[`./${currentRelativePath}`])
+
     globalPanel.webview.onDidReceiveMessage(
-        message => {
-            console.log(message)
+        msg => {
+            // Cache the change
+            dataJson[`./${currentRelativePath}`] = msg.text
+
+            // Save the change
+            saveFile(
+                vscode.Uri.file(context.workspaceState.get('config_fpath')), 
+                new TextEncoder().encode(JSON.stringify(dataJson, null, 2))
+            )
         },
         undefined,
         context.subscriptions
     );
-
-    globalPanel.onDidDispose(() => {
-        console.log('WebviewPanel disposed.');
-        globalPanel = null
-    });
 }
+
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+    const workspaceState = context.workspaceState;
 
-	let openFile = vscode.commands.registerCommand('code-meta-ext.openFile', function () {
-        let workspaceDir = vscode.workspace.workspaceFolders[0];
-        let fpath = vscode.Uri.joinPath(workspaceDir.uri, 's_config.json');
+    let configFpath = null
 
-        if (!globalPanel) {
-            panelId  = `code-meta-${uuidv4}`
+	let openNote = vscode.commands.registerCommand('code-meta-ext.openNote', async function () {
+        if(!workspaceState.get('config_fpath')) {
+            configFpath = await showFileDialog()
 
-            vscode.workspace.fs.readFile(fpath)
-            .then(content => {
-                configCache = content
-
-                init_panel(context)
-            })
-            .catch(error => {
-                console.error(error);
-            });
+            if(configFpath) {
+                workspaceState.update('config_fpath', configFpath, vscode.ConfigurationTarget.Workspace);
+            }else {
+                return
+            }
         }else {
-            globalPanel.reveal(vscode.ViewColumn.Two, true);
+            configFpath = workspaceState.get('config_fpath')
+        }
+
+        let workspaceDirs = vscode.workspace.workspaceFolders
+
+        if(workspaceDirs.length >  0) {
+            workspaceDir = workspaceDirs[0].uri;
+
+            let fpath = configFpath
+
+            if (!globalPanel) {
+                panelId = `code-meta-${uuidv4}`
+    
+                vscode.workspace.fs.readFile(vscode.Uri.file(fpath))
+                .then(content => {
+                    dataJson = JSON.parse(content.toString()); 
+
+                    initPanel(context)
+                })
+                .catch(error => {
+                    vscode.window.showErrorMessage('Failed to read datasoruce file')
+                    console.error(error);
+                });
+            }else {
+                globalPanel.reveal(vscode.ViewColumn.One, true);
+            }
+        }else {
+            vscode.window.showErrorMessage('No folder opened')
         }
 	});
 
-	let closeFile = vscode.commands.registerCommand('code-meta-ext.closeFile', function () {
-        globalPanel.dispose()
-	});
+    
+    let clearCache = vscode.commands.registerCommand(
+            'code-meta-ext.clearCache', 
+            function () {
+                workspaceState.update('config_fpath', null, vscode.ConfigurationTarget.Workspace);
+                vscode.window.showInformationMessage('Clear config file path cache.');
+            });
 
-	context.subscriptions.push(openFile, closeFile);
+	context.subscriptions.push(
+        openNote, 
+        clearCache
+        );
 }
 
-// This method is called when your extension is deactivated
+
 function deactivate() {}
+
 
 module.exports = {
 	activate,
 	deactivate
 }
+
